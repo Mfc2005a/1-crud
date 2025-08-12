7285bedf99e7f74213547574f466ee0c95cee3b0
Mateus ferrari Costa <mateusferrari@gmail.com>
2025-08-12 20:51:59 -0300

    1 site

---
+++ back/python/m.site.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_mail import Mail, Message
+import os
+
+app = Flask(__name__)
+
+#Configurações do Gmail 
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'mateusferrari.am@gmail.com'
+app.config['MAIL_PASSWORD'] = ''#pppp
+app.config['MAIL_DEFAULT_SENDER'] = ('Portifólio Dev', os.getenv('EMAIL_USER'))
+
+mail = Mail(app)
+
+#Rotas  
+@app.route("/")
+def homepage():
+    return render_template("Homapage.html")
+
+@app.route("/contatos", methods= ['get', 'post'])
+def contatos():
+    if request.method == 'Post':
+        nome = request.form['nome']
+        telefone = request.form['telefone']
+        gmail = request.form['gmail']
+        assunto = request.form['assunto']
+
+        corpo_email = f"""
+        Novo contato recebido:
+
+        Nome: {nome}
+        Telefone: {telefone}
+        Gmail: {gmail}
+        Assunto: {assunto}
+        """
+        msg = Message(subject="Novo contato :)", recipients=[os.getenv('EMAIL_USER')], body=corpo_email)
+        mail.send(msg)
+
+        
+        return redirect(url_for('homepage.html'))
+    
+    return render_template("contatos.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ static/js/app.js
@@ -0,0 +1,58 @@
+alert("Qualquer coisa");
+
+let index = 0; 
+        const titles = [ 
+            "Mateus Ferrari Costa",
+            "Desenvolvedor Full stack",
+        ]; 
+        function changeTitle(index, delay) {
+            
+            setInterval(() => {
+                document.querySelector("h1").textContent = titles[index];
+                index = (index + 1) % titles.length; // Retorna ao início
+            }, delay);
+        }
+        changeTitle(index, 3000); //  
+    
+        document.querySelector(".stack").addEventListener("click", () => {
+
+            const p = document.querySelector(".stack");
+            p.style.color = "#00ff00"; // texto verde 
+            p.style.backgroundColor = "#000;" // fundo preto
+            p.style.padding = "10px" 
+        });
+        
+        const topBtn = document.getElementById("topBtn")
+
+        window.onscroll = () => {
+            if (window.scrollY > 100) {
+                topBtn.style.display = "block" ;
+}           else {
+                topBtn.style.display = "none" ; 
+            }
+        };
+
+        topBtn.onclick = () => {
+            window.scrollTo({top: 0, behavior: "smooth"});
+        
+        };
+
+        const stack = document.querySelector(".stack")
+        stack.classList.add("fade-in")
+        setTimeout(() => {
+            stack.classList.add("visible");
+
+        }, 100);
+        
+        const titulo = [
+            
+        ]
+
+        let indice = 0 
+        const h1 = document.getElementById("titulo");
+
+        setInterval(() => {
+            indice = (indice + 1 ) % titulos.length; // Retorna ao inicio 
+            h1.textContent = titulo[indice];
+
+        }, 5000);
